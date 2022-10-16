54c2a01ff1c9ae51f5f478b8201e3900dfed4034
Kosta Kazakov <kostakazakoff@gmail.com>
2022-10-16 16:01:10 +0300

    Springtime code optimising

--- advanced_exam_preparation/springtime.py
+++ advanced_exam_preparation/springtime.py
@@ -1,15 +1,18 @@
 def start_spring(**kwargs):
     objects_dict = {}
     output = ''
+
     for k, v in kwargs.items():
         if v not in objects_dict:
             objects_dict[v] = []
         objects_dict[v].append(k)
+
     for k, v in dict(sorted(objects_dict.items(), key=lambda x: (-len(x[1]), x[0]))).items():
         output += f'{k}:\n'
         if v:
             v = "\n-".join(sorted(v))
             output += f'-{v}\n'
+            
     return output
 
 
