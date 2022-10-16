2cf3aab149c73d0728e4383165538d51318d4ff1
Kosta Kazakov <kostakazakoff@gmail.com>
2022-10-16 13:35:47 +0300

    Pown Wars code optimisation

--- advanced_exam_preparation/pawn_wars.py
+++ advanced_exam_preparation/pawn_wars.py
@@ -1,34 +1,40 @@
 def player_moves():
-    global r, c, color, chessboard, players
+    global r, c, color, chessboard, player
     chessboard[r][c] = '-'
+
     if color == 'White':
         player_step = r - 1
         end_of_board = 0
         diags_of_attack = ((r - 1, c + 1), (r - 1, c - 1))
+
     else:
         player_step = r + 1
         end_of_board = 7
         diags_of_attack = ((r + 1, c + 1), (r + 1, c -1))
+
     return end_of_board, diags_of_attack, player_step
 
 
-players = {'White': [0, 0], 'Black': [0, 0]}
+player = {'White': [0, 0], 'Black': [0, 0]}
 col_marks = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h'}
 turn = ['White', 'Black']
 chessboard = []
 
 for row in range(8):
     rank = input().split()
+
     if 'w' in rank:
-        players['White'][0], players['White'][1] = row, rank.index('w')
+        player['White'][0], player['White'][1] = row, rank.index('w')
         rank[rank.index('w')] = 'White'
+
     if 'b' in rank:
-        players['Black'][0], players['Black'][1] = row, rank.index('b')
+        player['Black'][0], player['Black'][1] = row, rank.index('b')
         rank[rank.index('b')] = 'Black'
+        
     chessboard.append(rank)
 
 while True:
-    r, c = players[turn[0]][0], players[turn[0]][1]
+    r, c = player[turn[0]][0], player[turn[0]][1]
     color = turn[0]
     oponent_color = turn[1]
     end, diag_attack, step = player_moves()
@@ -42,7 +48,7 @@ while True:
             print(f"Game over! {color} win, capture on {col_marks[d[1]]}{8 - d[0]}.")
             exit()
 
-    players[color][0] = step
+    player[color][0] = step
     chessboard[step][c] = color
 
     turn[0], turn[1] = turn[1], turn[0]
