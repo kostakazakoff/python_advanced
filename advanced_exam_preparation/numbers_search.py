8d2e094eade317559865773576751a56555904c3
Kosta Kazakov <kostakazakoff@gmail.com>
2022-10-20 18:29:28 +0300

    "Numbers Search" problem solved

--- advanced_exam_preparation/numbers_search.py
+++ advanced_exam_preparation/numbers_search.py
@@ -1,7 +1,10 @@
 def numbers_searching(*args):
     numbers = [int(x) for x in args]
     smallest, biggest = min(numbers), max(numbers)
-    
+    sequence = [x for x in range(smallest, biggest + 1)]
+    missing_number = list(set(sequence) - set(numbers))[0]
+    duplicates = sorted(list(set([x for x in numbers if numbers.count(x) > 1])))
+    return [missing_number, duplicates]
 
 
-print(numbers_searching(1, 2, 4, 2, 5, 4))
+print(numbers_searching(50, 50, 47, 47, 48, 45, 49, 44, 47, 45, 44, 44, 48, 44, 48))
