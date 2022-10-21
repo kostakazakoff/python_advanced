0a896fa580af56db8174c40936d3db6ce22406a9
Kosta Kazakov <kostakazakoff@gmail.com>
2022-10-21 20:49:43 +0300

    repairing

--- multidimensional_lists/bombs_2_unfinished.py
+++ multidimensional_lists/bombs_2_unfinished.py
@@ -8,7 +8,7 @@ for (r, c) in BOMB_COORDINATES:
     matrix[r][c] = 0
     for direction in ((-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)):
         row, col = r + direction[0], c + direction[1]
-        if 0 <= row < size and 0 <= col < size and matrix[row][col] > 0 and matrix[row][col] > 0:
+        if 0 <= row < size and 0 <= col < size and matrix[row][col] > 0:
             matrix[row][col] -= power
 
 [[alive_cells.append(n) for n in row if n > 0] for row in matrix]
